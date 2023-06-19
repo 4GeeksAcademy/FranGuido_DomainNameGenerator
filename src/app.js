6607db5b2579497a1fe75979dcba28db873ed326
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-19 22:17:56 +0000

    coded for nested loops

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let adjective = ["kawaii", "thicc", "sticky"];
+let noun = ["donuts", "mochi", "bubbletea"];
+let domain = [".com", ".cr", ".int"];
+
+for (let i = 0; i < adjective.length; i++) {
+  for (let j = 0; j < noun.length; j++) {
+    for (let k = 0; k < domain.length; k++) {
+      let div = document.createElement("div");
+      div.innerText = adjective[i] + noun[j] + domain[k];
+      document.body.appendChild(div);
+    }
+  }
+}
+
+console.log("${adjective[i]}${noun[j]}${domain[k]}");
