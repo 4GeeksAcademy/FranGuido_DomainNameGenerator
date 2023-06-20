09f7daf10df0c82ed9a9538488757277bf787970
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-20 00:55:28 +0000

    optimized the code (now is shorter)

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onclick = function() {
+
   //write your code here
 
   let adjective = ["kawaii", "thicc", "sticky"];
   let noun = ["donuts", "mochi", "bubbletea"];
   let domain = [".com", ".cr", ".int"];
 
+  
+
   for (let i = 0; i < adjective.length; i++) {
     for (let j = 0; j < noun.length; j++) {
       for (let k = 0; k < domain.length; k++) {
-        let div = document.createElement("div");
-        div.innerText = adjective[i] + noun[j] + domain[k];
-        document.body.appendChild(div);
+        let Domain = adjective[i] + noun[j] + domain[k];
+        console.log(Domain);
       }
     }
-  }
-};
+  };
+
+
